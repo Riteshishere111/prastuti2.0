9064f41401c527b2c04590980af84caf9691b590
Ritesh <officialriteshwork@gmail.com>
2025-09-05 08:20:24 +0530

    Refresh

--- Bca_automaton_result/Learning/python/3.List_dictionaries.py
+++
@@ -1,6 +0,0 @@
-name= "Ritesh"
-age = 28
-
-subjects = ["Math","Physcis", "CS"] # List
-student = {"name": "Ritesh", "age": 22}  #dictionary
-print(name, age)
--- Bca_automaton_result/Learning/python/6.Use_try-except_ERROR_handling.py
+++
@@ -1,7 +0,0 @@
-try:
-    num=int(input("Enter a number: "))
-    print(10/num) #This might cause an error is num is 0
-except ZeroDivisionError:
-    print("Cannot Divide by Zero!")
-except ValueError:
-    print("Invalid input! Please enter a number.")
